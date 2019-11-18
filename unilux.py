1227495c3949f0b5596443d7d6c762529dbccc0a
kahstro <viniciusm0r3or4@gmail.com>
2019-11-18 17:01:33 -0300

    bd user adicionado

--- unilux.py
+++ unilux.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///unilux.db'
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20), nullable=False)
+    sobrenome = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    senha = db.Column(db.String(30), nullable=False)
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__== '__main__':
     app.run(debug=True) 
